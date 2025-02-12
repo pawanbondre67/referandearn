e58b482bdf44e561f5a8c115e7e16ad5b7869a36
pawanbondre67 <bondrepavan623@gmail.com>
2025-02-12 18:52:01 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReferEarn from './components/ReferEarn';
+const  App : React.FC = ()=>{
+  return (
+    <>
+    <div className='bg-amber-300'>App</div>
+
+    <ReferEarn />
+    </>
+  )
+}
+
+export default App
